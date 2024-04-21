ca56c09cac17fdc1b9c049e71c92922727edd128
Gandhar04 <121543341+Gandhar04@users.noreply.github.com>
2024-04-21 18:45:00 +0000

    math and num

---
+++ 01_basics/04_string.js
@@ -0,0 +1,21 @@
+const name = "Gandhar"
+const repoCount =50
+
+// console.log(name + repoCount+ " value");
+
+console.log(`Hellow my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("sopal");
+
+console.log(gameName[2]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+
+const newString = gameName.substring(1,3)
+console.log(newString);
+
+const url = "https://gandharsopal.com"
+console.log(url.replace('gandhar', '-'))
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,5 @@
+memory
+
+Stack (primitve), Heap (Non-rimitive)
+
+let  myYoutubename = "Gandharsopaldotcom"
