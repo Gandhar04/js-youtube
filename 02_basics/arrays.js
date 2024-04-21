2a800c851ed3349e2a29995e330262603c7fb6d5
Gandhar04 <121543341+Gandhar04@users.noreply.github.com>
2024-04-21 19:27:36 +0000

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,21 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5, 6]
+const myHeroes = ["skaktiman", "nagraj"]
+console.log(myArr[0]);
+
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr2[1]);
+
+
+// .pop == last value remove
+// .unshift(0) add first
+// .shif() fisrst remove
+
+const newArr = myArr.join()  // joins 2 arrya and changes type to string
+
+//slice,spice
+console.log("A ", myArr);
+console.log(myArr.slice(1,3));
+console.log(myArr.splice(1,3))
+console.log(myArr);
